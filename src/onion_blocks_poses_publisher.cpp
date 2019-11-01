73e555988705f72886c80680a3e457490c5d7565
Prasanth Sengadu Suresh <prasuchit@gmail.com>
2019-11-01 12:35:11 -0400

    Merged two_scara_collaboration with this pkg

---
+++ src/onion_blocks_poses_publisher.cpp
@@ -0,0 +1,162 @@
+// this node will publish the topic "onions_blocks_poses"
+// including all current onions blocks, pose is 3-D position
+
+// ros communication:
+    // subscribe to topic "/current_onions_blocks"
+    // subscribe to topic "/gazebo/model_states"
+    // publish the topic "/onions_blocks_poses"
+
+#include <ros/ros.h>
+#include <vector>
+#include <string>
+#include <std_msgs/Int8MultiArray.h>
+#include <gazebo_msgs/ModelStates.h>
+#include <sawyer_irl_project/onions_blocks_poses.h>
+#include <gazebo_msgs/SetModelState.h>
+using namespace std;
+// global variables
+int g_onions_quantity;
+vector<int8_t> g_current_onions_blocks;
+vector<double> g_x;
+vector<double> g_y;
+vector<double> g_z;
+bool g_current_onions_callback_started = false;
+bool g_onions_poses_updated = false;  // act as frequency control of publish loop
+bool call_service;
+double SAWYERRANGE_UPPER_LIMIT,initial_pose_x, initial_pose_y, initial_pose_z;
+ros::ServiceClient setModelState;
+gazebo_msgs::SetModelState model_state_srv_msg;
+vector<int> indices_deleted;
+int goodonionsConvEnd=0;
+int badonionsConvEnd=0;
+int goodonionsInBin=0;
+int badonionsInBin=0;
+
+string intToString(int a) {
+    stringstream ss;
+    ss << a;
+    return ss.str();
+}
+
+void currentonionsCallback(const std_msgs::Int8MultiArray& current_onions_blocks) {
+    // this topic contains information of what onions blocks have been spawned
+    cout << "\nReceived ros msg on /current_onions_blocks in file onions_blocks_poses_publisher.cpp";
+    if (!g_current_onions_callback_started) {
+        // set first time started flag to true
+        g_current_onions_callback_started = true;
+        ROS_INFO("\nCurrent onions callback has been invoked first time");
+    }
+    g_onions_quantity = current_onions_blocks.data.size();
+    cout<<"\nNumber of onions currently available: "<<g_onions_quantity<<endl;
+    g_current_onions_blocks.resize(g_onions_quantity);
+    g_current_onions_blocks = current_onions_blocks.data;   //This is the color variable we use 
+                                                            //to differentiate good and bad onions
+}
+
+void modelStatesCallback(const gazebo_msgs::ModelStates& current_model_states/*, gazebo_msgs::SetModelState& model_state_srv_msg*/) {
+    // this callback update global values of onions positions
+    if (g_current_onions_callback_started) {
+        // only update when currentonionsCallback has been invoked the first time
+        // get onions blocks positions according to settings in g_current_onions_blocks
+        vector<double> onions_x;
+        vector<double> onions_y;
+        vector<double> onions_z;
+        onions_x.resize(g_onions_quantity);
+        onions_y.resize(g_onions_quantity);
+        onions_z.resize(g_onions_quantity);
+        // find position of all current onions in topic message
+        bool onions_poses_completed = true;
+        int ind_del, prev_index = -1;
+        for (int i = 0; i < g_onions_quantity; i++) {
+            
+            // get index of ith onions
+            string indexed_model_name;
+            if (g_current_onions_blocks[i] == 0) {  //If color variable is 0, good onion else bad
+                indexed_model_name = "good_onion_" + intToString(i);
+            }
+            else {
+                indexed_model_name = "bad_onion_" + intToString(i);
+            }
+            int index = -1;
+            int model_quantity = current_model_states.name.size();  // number of models measured
+            for (int j = 0; j < model_quantity; j++) {
+                if (current_model_states.name[j] == indexed_model_name) {
+                    index = j; 
+                    break;
+                }
+            }
+            if (index != -1) {
+                // this model name exists and has been successfully indexed
+                onions_x[i] = current_model_states.pose[index].position.x;
+                onions_y[i] = current_model_states.pose[index].position.y;
+                onions_z[i] = current_model_states.pose[index].position.z;
+
+                // update global counter for onions in Bin and delete them
+                
+                // update global counter for onions reaching end of conveyor and delete them
+                if (onions_y[i] >= SAWYERRANGE_UPPER_LIMIT && onions_z[i] <= initial_pose_z + 1) {
+
+                    if (g_current_onions_blocks[i] == 0) {
+                        goodonionsConvEnd += 1;
+                    } else {
+                        badonionsConvEnd += 1;
+                    }
+                    //hard code, or could prompt, or could have command-line arg here:
+                    model_state_srv_msg.request.model_state.model_name = indexed_model_name;
+                    model_state_srv_msg.request.model_state.pose.position.x = initial_pose_x; 
+                    model_state_srv_msg.request.model_state.pose.position.y = initial_pose_y - i*0.009; //Just creating a small difference b/w their positions
+                    model_state_srv_msg.request.model_state.pose.position.z = initial_pose_z;
+                    setModelState.call(model_state_srv_msg);
+                        //make sure service call was successful
+                        bool result = model_state_srv_msg.response.success;
+                        if (!result)
+                            ROS_WARN("service call to set_model_state failed!");
+                        else
+                            ROS_INFO_STREAM("Model state changed: "<<indexed_model_name);    
+                } 
+            }
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "onions_blocks_poses_publisher");
+    ros::NodeHandle nh;
+
+    // initialize subscribers for "/current_onions_blocks" and "/gazebo/model_states"
+    ros::Subscriber current_onions_subscriber = nh.subscribe("/current_onions_blocks"
+        , 1, currentonionsCallback);
+    setModelState = nh.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+    //Leave the next line for educational purposes
+    //ros::Subscriber model_states_subscriber = nh.subscribe("/gazebo/model_states", 1, boost::bind(modelStatesCallback, _1, model_state_srv_msg));
+    ros::Subscriber model_states_subscriber = nh.subscribe("/gazebo/model_states", 1, modelStatesCallback);
+    // initialize publisher for "/onions_blocks_poses"
+    ros::Publisher onions_poses_publisher
+        = nh.advertise<sawyer_irl_project::onions_blocks_poses>("onions_blocks_poses", 1);
+    sawyer_irl_project::onions_blocks_poses current_poses_msg;
+
+    nh.getParam("/SAWYERRANGE_UPPER_LIMIT", SAWYERRANGE_UPPER_LIMIT);
+    nh.getParam("/initial_pose_x", initial_pose_x);
+    nh.getParam("/initial_pose_y", initial_pose_y);
+    nh.getParam("/height_spawning", initial_pose_z);
+    // publishing loop
+    while (ros::ok()) {
+        if (g_onions_poses_updated) {
+            // only publish when onions positions are updated
+            // no need to publish repeated data
+            g_onions_poses_updated = false;  // set flag to false
+            // there is tiny possibility that g_x is not in the length of g_onions_quantity
+            int local_onions_quantity = g_x.size();  // so get length of g_x
+            current_poses_msg.x.resize(local_onions_quantity);
+            current_poses_msg.y.resize(local_onions_quantity);
+            current_poses_msg.z.resize(local_onions_quantity);
+            current_poses_msg.x = g_x;
+            current_poses_msg.y = g_y;
+            current_poses_msg.z = g_z;
+            onions_poses_publisher.publish(current_poses_msg);
+        }
+        // rate_timer.sleep();
+        ros::spin();
+    }
+    return 0;
+}
---
+++ src/onion_blocks_spawner.cpp
@@ -0,0 +1,163 @@
+// Author: Prasanth Suresh(ps32611@uga.edu); 
+// Description: We  have good onions and bad onions. 
+// Aim of this script is to spawn them on the conveyor surface.
+
+//STILL UNDER CONSTRUCTION!
+// Do not edit/copy without permission.
+
+#include <ros/ros.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <gazebo_msgs/SpawnModel.h>
+#include <std_msgs/Int8MultiArray.h>
+#include <std_srvs/Trigger.h>
+#include <sawyer_irl_project/StringArray.h>
+
+using namespace std;
+// int to string converter
+string intToString(int a) {
+    stringstream ss;
+    ss << a;
+    return ss.str();
+}
+
+int main(int argc, char **argv) {
+
+ros::init(argc, argv, "onion_blocks_spawner");
+    ros::NodeHandle nh; //("~");
+    int i = 0 /*index the onions*/,j = 0; 
+    double initial_pose_x, initial_pose_y, height_spawning, spawning_interval, 
+        conveyor_center_x, belt_width,wrench_duration, randpos, object_width;;
+    bool spawn_multiple;
+    string good_onion_path,bad_onion_path,OnionCenter_path,Onion0_path, 
+        good_xmlStr, bad_xmlStr, OnionCenter_xmlStr, Onion0_xmlStr, model_name;
+    int8_t color_index;  // 0 is good, 1 is bad
+    ifstream good_inXml, bad_inXml;
+    stringstream good_strStream, bad_strStream, OnionCenter_strStream, Onion0_strStream;
+    nh.getParam("/initial_pose_x", initial_pose_x);
+    nh.getParam("/initial_pose_y", initial_pose_y);
+    nh.getParam("/height_spawning", height_spawning);
+    nh.getParam("/spawning_interval", spawning_interval);
+    nh.getParam("/conveyor_center_x", conveyor_center_x);
+    nh.getParam("/belt_width", belt_width);
+    nh.getParam("/spawn_multiple", spawn_multiple);
+    nh.getParam("/object_width", object_width);
+	// get file path of onions from parameter server
+    bool get_good_onion_path, get_bad_onion_path, get_OnionCenter_path, get_Onion0_path;
+    get_good_onion_path = nh.getParam("/good_onion_path", good_onion_path);
+    get_bad_onion_path = nh.getParam("/bad_onion_path", bad_onion_path);
+    get_OnionCenter_path = nh.getParam("/OnionCenter_path", OnionCenter_path);
+    get_Onion0_path = nh.getParam("/OnionCenter_path",Onion0_path);
+
+    ros::ServiceClient spawn_model_client
+        = nh.serviceClient<gazebo_msgs::SpawnModel>("/gazebo/spawn_sdf_model");
+    gazebo_msgs::SpawnModel spawn_model_srv_msg;  // service message
+    // service client for service /gazebo/apply_body_wrench
+	// publisher for /current_onions
+    ros::Publisher current_onions_publisher
+        = nh.advertise<std_msgs::Int8MultiArray>("current_onions_blocks", 1);
+    std_msgs::Int8MultiArray current_onions_msg;
+    current_onions_msg.data.clear();
+
+    // publisher for /modelnames
+    ros::Publisher modelnames_publisher
+        = nh.advertise<sawyer_irl_project::StringArray>("modelnames", 1);
+    sawyer_irl_project::StringArray modelnames_msg;
+    modelnames_msg.modelnames.clear();
+	// make sure /gazebo/spawn_sdf_model service is ready
+    bool service_ready = false;
+    while (!service_ready) {
+        service_ready = ros::service::exists("/gazebo/spawn_sdf_model", true);
+        ROS_INFO("waiting for spawn_sdf_model service");
+        ros::Duration(0.5).sleep();
+    }
+    ROS_INFO("spawn_sdf_model service is ready");
+
+    if (!(get_good_onion_path && get_bad_onion_path && get_OnionCenter_path && get_Onion0_path))
+    {
+        ROS_INFO("failed to get parameters");
+        return 0;  // return if fail to get parameters
+    }
+
+	// prepare the xml for service call, read sdf into string
+    // good onion
+    good_inXml.open(good_onion_path.c_str());
+    good_strStream << good_inXml.rdbuf();
+    good_xmlStr = good_strStream.str();
+    // bad onion
+    bad_inXml.open(bad_onion_path.c_str());
+    bad_strStream << bad_inXml.rdbuf();
+    bad_xmlStr = bad_strStream.str();
+	
+	ifstream OnionCenter_inXml(OnionCenter_path.c_str());
+    OnionCenter_strStream << OnionCenter_inXml.rdbuf();
+    OnionCenter_xmlStr = OnionCenter_strStream.str();
+    ifstream Onion0_inXml(Onion0_path.c_str());
+    Onion0_strStream << Onion0_inXml.rdbuf();
+    Onion0_xmlStr = Onion0_strStream.str();
+	// prepare the spawn model service message
+    spawn_model_srv_msg.request.initial_pose.position.x = initial_pose_x; // 4.0
+    spawn_model_srv_msg.request.initial_pose.position.y = initial_pose_y; // 4.0
+    spawn_model_srv_msg.request.initial_pose.position.z = height_spawning; // 0.2 ; on the conveyor belt
+	spawn_model_srv_msg.request.initial_pose.orientation.x = 0.0;
+    spawn_model_srv_msg.request.initial_pose.orientation.y = 0.0;
+    spawn_model_srv_msg.request.initial_pose.orientation.z = 0.0;
+    spawn_model_srv_msg.request.initial_pose.orientation.w = 1.0;
+    spawn_model_srv_msg.request.reference_frame = "world";
+
+	//begin spawning onions and give an initial speed on conveyor
+
+    ros::Duration(2).sleep();
+    while (ros::ok()) {
+        //SPAWNING AND MOVING THREE OBJECTS
+
+        while(i < 4) {
+            string index_string = intToString(i);
+            ++j;
+            spawn_model_srv_msg.request.initial_pose.position.x = 0.87 - j*0.05; //width of sphere is 0.02, well within 0.05
+            spawn_model_srv_msg.request.initial_pose.position.y = -0.45 - j*0.05; //width of sphere is 0.02, well within 0.05
+            ROS_INFO_STREAM("x position of new onion: "
+                << spawn_model_srv_msg.request.initial_pose.position.x);
+            ROS_INFO_STREAM("y position of new onion: "
+                << spawn_model_srv_msg.request.initial_pose.position.y);
+                
+                color_index = 0;
+                model_name = "good_onion_" + index_string;  // initialize model_name
+                spawn_model_srv_msg.request.model_name = model_name;
+                spawn_model_srv_msg.request.robot_namespace = "good_onion_" + index_string;
+                spawn_model_srv_msg.request.model_xml = good_xmlStr;
+
+            // call spawn model service
+            bool call_service = spawn_model_client.call(spawn_model_srv_msg);
+            if (call_service) {
+                if (spawn_model_srv_msg.response.success) {
+                    ROS_INFO_STREAM(model_name << " has been spawned");
+                }
+                else {
+                    ROS_INFO_STREAM(model_name << " spawn failed");
+                }
+            }
+            else {
+                ROS_INFO("fail in first call");
+                ROS_ERROR("fail to connect with gazebo server");
+                return 0;
+            }
+            // publish current onion blocks status, all onion blocks will be published
+            // no matter if it's successfully spawned, or successfully initialized in speed
+            current_onions_msg.data.push_back(color_index);
+            current_onions_publisher.publish(current_onions_msg);
+            modelnames_msg.modelnames.push_back(spawn_model_srv_msg.request.model_name);
+            modelnames_publisher.publish(modelnames_msg);
+            // loop end, increase index by 1
+            i++;
+        }
+        ros::Duration(5).sleep();
+    //ROS_INFO_STREAM("");	//Debug print statement
+    ros::spin();
+    ros::Duration(spawning_interval).sleep(); // frequency control, spawn one onion in each loop
+    // delay time decides density of the onions
+    }
+return 0;
+}
